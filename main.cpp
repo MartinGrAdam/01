1bdc79233aa2e099085eed6450744f5a89d1d654
Martin Adam <martinad@example.com>
2020-05-20 20:54:27 +0200

    Zmian w pliku 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <unistd.h>
 using namespace std;
 
 int main() {
@@ -7,11 +8,12 @@ int main() {
 //1 zmienna srodowiska: OMP_NUM_THREADS
 //2 funkcja biblioteczna: omp_set_num_threads(int)
 //3 klauzura: numthreads(int)
-
+ 
 #pragma omp parallel num_threads(4)
 {
     int ID = omp_get_thread_num();
     cout << "Serdecznie Witamy!!!" << ID << endl;
-
-}
-}
+    }
+    
+   int usleep (1000000);
+    }
