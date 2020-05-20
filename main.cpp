8e5d985d212929ee4e6d99bdbdb09b5bc73e9978
Martin Adam <martinad@example.com>
2020-05-20 19:52:03 +0200

    Pierwsze repozytorium

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+//mozliwosc ustawiania liczby watkow
+//1 zmienna srodowiska: OMP_NUM_THREADS
+//2 funkcja biblioteczna: omp_set_num_threads(int)
+//3 klauzura: numthreads(int)
+
+#pragma omp parallel num_threads(4)
+{
+    int ID = omp_get_thread_num();
+    cout << "Serdecznie Witamy!!!" << ID << endl;
+
+}
+}
